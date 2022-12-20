d8da9747f8150f6bbd5df55ba53ba4906734316a
Daniel Kristio Utomo <79684620+Danielkristu@users.noreply.github.com>
2022-12-20 19:27:44 +0700

    initial commit

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=1791d2da71e8f9814dac137d3cede807";
+
+  return <div className="app"></div>;
 }
 
 export default App;
+
+//1791d2da71e8f9814dac137d3cede807
